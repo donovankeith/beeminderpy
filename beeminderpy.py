59a8ecfb0b64b4907a5311dd572dd4978862a630
Donovan Keith <donovanskeith@gmail.com>
2015-02-18 13:56:06 -0800

    Made username and goalname members of the Beeminder object. You can still change them directly w/ the membernames but this cuts down on variable passing. I've also updated the call_api method so that you automatically get a dict and don't have to decode the JSON yourself.

--- beeminderpy.py
+++ beeminderpy.py
@@ -1,49 +1,52 @@
 import urllib
 import urllib2
 import settings
+import json
 
 # based on https://www.beeminder.com/api
 
 debug = True
 
 class Beeminder:
-    def __init__(self, this_auth_token=None):
+    def __init__(self, this_auth_token=settings.BEEMINDER_AUTH_TOKEN, username=settings.BEEMINDER_USERNAME, goalname=""):
         self.auth_token = this_auth_token
+        self.username = username
+        self.goalname = goalname
         self.base_url = 'https://www.beeminder.com/api/v1'
 
-    def get_user(self, username=settings.BEEMINDER_USERNAME):
+    def get_user(self):
         """Return user data string.
         """
 
         if debug:
-            print "get_user(username='%s')" % username
+            print "get_user()"
 
-        url = "%s/users/%s.json" % (self.base_url, username)
+        url = "%s/users/%s.json" % (self.base_url, self.username)
         values = {'auth_token': self.auth_token}
         result = self.call_api(url, values, 'GET')
         return result
 
-    def get_goal(self, username, goalname):
+    def get_goal(self):
         """Return
         """
 
         if debug:
-            print "get_goal(username='%s', goalname='%s')" % (username, goalname)
+            print "get_goal(username='%s', goalname='%s')" % (self.username, self.goalname)
 
-        url = "%s/users/%s/goals/%s.json" % (self.base_url, username, goalname)
+        url = "%s/users/%s/goals/%s.json" % (self.base_url, self.username, self.goalname)
         values = {'auth_token': self.auth_token}
         result = self.call_api(url, values, 'GET')
         return result
 
-    def get_datapoints(self, username, goalname):
+    def get_datapoints(self):
         """Return
         """
 
         if debug:
-            print "get_datapoints(username='%s', goalname='%s')" % (username, goalname)
+            print "get_datapoints()"
 
-        url = self.base_url + 'users/' + username + '/goals/' + goalname + '/datapoints.json'
-        url = "%s/users/%s/goals/%s/datapoints.json" % (self.base_url, username, goalname)
+        url = self.base_url + 'users/' + self.username + '/goals/' + self.goalname + '/datapoints.json'
+        url = "%s/users/%s/goals/%s/datapoints.json" % (self.base_url, self.username, self.goalname)
         values = {'auth_token': self.auth_token}
         result = self.call_api(url, values, 'GET')
         return result
@@ -81,4 +84,5 @@ class Beeminder:
         else:
             response = urllib2.urlopen(url + '?' + data)
         result = response.read()
-        return result
+        result_dict = json.loads(result)
+        return result_dict
--- weekendsoff.py
+++ weekendsoff.py
@@ -6,9 +6,12 @@ import time
 import sys
 
 def main():
-    bm = beeminderpy.Beeminder()
+    bm = beeminderpy.Beeminder(goalname="cloth")
     user_data = bm.get_user()
     goal_data = bm.get_goal()
+
+    print user_data
+    print goal_data
 
 if __name__ == '__main__':
     main()
